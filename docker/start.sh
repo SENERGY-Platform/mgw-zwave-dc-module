03ffb954f194261e7a1cce492ca284a2ba142972
Ingo <roessner@infai.org>
2025-08-04 08:58:25 +0200

    add additional zwave2mqtt env variables

--- docker/start.sh
+++ docker/start.sh
@@ -7,4 +7,20 @@ jq '.mqtt.host = env.MQTT_BROKER_HOST' /usr/src/app/store/settings.json | sponge
 # use argjson to set port as number
 jq --argjson port ${MQTT_BROKER_PORT} '.mqtt.port = $port' /usr/src/app/store/settings.json | sponge /usr/src/app/store/settings.json
 
+# logs
+# gateway
+jq --argjson value ${GATEWAY_LOG_ENABLED} '.gateway.logEnabled = $value' /usr/src/app/store/settings.json | sponge /usr/src/app/store/settings.json
+jq '.gateway.logLevel = env.GATEWAY_LOG_LEVEL' /usr/src/app/store/settings.json | sponge /usr/src/app/store/settings.json
+# zwave
+jq --argjson value ${ZWAVE_LOG_ENABLED} '.zwave.logEnabled = $value' /usr/src/app/store/settings.json | sponge /usr/src/app/store/settings.json
+jq '.zwave.logLevel = env.ZWAVE_LOG_LEVEL' /usr/src/app/store/settings.json | sponge /usr/src/app/store/settings.json
+
+# qos
+jq --argjson value ${MQTT_QOS} '.mqtt.qos = $value' /usr/src/app/store/settings.json | sponge /usr/src/app/store/settings.json
+
+# mqtt topic prefix
+jq '.mqtt.prefix = env.MQTT_TOPIC_PREFIX' /usr/src/app/store/settings.json | sponge /usr/src/app/store/settings.json
+
+cat /usr/src/app/store/settings.json
+
 node server/bin/www
